dbef1b8078bc56a5b429c63412ba86ad8abcb28a
DiegelD <52889569+DiegelD@users.noreply.github.com>
2021-02-21 13:24:16 +0100

    Add files via upload

---
+++ Car.cpp
@@ -0,0 +1,25 @@
+
+#include "Car.h"
+
+using std::string;
+using std::vector;
+
+// Initializes Vehicle
+Car::Car()
+{
+    int lane = 1;
+}
+
+Car::~Car()
+    {
+    }
+
+void Car::update(double x_in, double y_in, double s_in, double d_in, double yaw_in, double speed_in)
+    {
+        double x = x_in;
+        double y = y_in;
+        double s = s_in;
+        double d = d_in;
+        double yaw = yaw_in;
+        double speed = speed_in;
+    }
